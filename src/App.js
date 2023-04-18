b04629e14747366737ae2a268b6847a5f2661b21
AlexDerAndros <ntrikos@gmx.de>
2023-04-18 23:38:42 +0200

    ss

--- src/App.js
+++ src/App.js
@@ -12,6 +12,9 @@ export default function App() {
     </div>
     </main>
   );
+  // Aktualisierung einer React App: 1.npm run build 2.firebase deploy
+  // 3.Quellcodeverwaltung unten dann oben commit 4. Aktualisieren
+  // Wichtig: immer alles auf der neusten Version und updaten ab und zu
 }
 function Home() {
   return (
