507bb593e0ca4cc341bed0b2c622019d6970b359
AlexDerAndros <ntrikos@gmx.de>
2023-04-23 23:07:21 +0200

    d

--- src/App.js
+++ src/App.js
@@ -4,29 +4,63 @@ import './App.css';
 export default function App() {
   return (
     <main>
-    <div className='home'>
-    <Home/>
-    </div>
-    <div className='ham'>
-      <Hamburger/>
-    </div>
+    <Header/>
     </main>
   );
   // Aktualisierung einer React App: 1.npm run build 2.firebase deploy
   // 3.Quellcodeverwaltung unten dann oben commit 4. Aktualisieren
   // Wichtig: immer alles auf der neusten Version und updaten ab und zu
 }
-function Home() {
+function Header() {
   return (
-  <header>
-    Home
-  </header>
+    <div className='header'>
+    <Hamburger/>
+      <div className='title' >
+      <a href='https://michael-ntrikos.art'>
+      Artist Michael Ntrikos 
+      </a>
+      </div>
+    </div>
   );
 }
 function Hamburger() {
+  const [click,setClick] = useState(false);
+  
+  const press = () => {
+    setClick(!click);
+  };
   return (
-    <div>
-      Hamburger Menu
+ <div className='ham'>
+   <div onClick={press} className='click'>
+    <div className='linie1'></div>
+    <div className='linie2'></div>
+    <div className='linie3'></div>
+   </div>
+ { click && (
+  <div className='content'>
+    <div style={{color:'white'}}>
+     hallo
     </div>
+  </div>
+ )}
+ </div>
   );
+}
+function Main() {
+ return (
+  <div className='Mitte'>
+ <div className='info1'> 
+ I'm an artist
+</div>
+
+  </div>
+ );
+}
+function Bottom() {
+return (
+ <div className='unten'>
+ 
+
+ </div>
+);
 }
