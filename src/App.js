fa67ff27736e6590dad71d0340275eecd9b2524d
AlexDerAndros <ntrikos@gmx.de>
2023-12-03 11:08:41 +0100

    w

--- src/App.js
+++ src/App.js
@@ -1676,11 +1676,15 @@ const videos = ['./C30BCC52-6700-4FCF-9B90-167169F37065.mp4',
  './85210F06-83A1-4F7C-85C7-0C2475B1FE93.mp4','./B4B3E89B-E02F-4735-97EC-D467C10D7874.mp4',
 './4F2D5055-EED0-498C-BDCD-FD2F4DC7D910.mp4', 
 './copy_B0D61099-C60A-4D60-9B07-08C4248CD82B.mp4','./B385F5F1-CA88-490F-9CA3-61E6C5B166EC.mp4',
-'./A02876CC-9293-48F9-9B32-127EDD12F151.mp4','./export_1683919611553.mp4',
+'./A02876CC-9293-48F9-9B32-127EDD12F151.mp4','./export_1683919611553.MP4',
 './41999F3E-67B9-4AD4-BCF4-90C304F84E04.mp4', "./D9EFDC13-AA25-4747-AA3C-587C5C6461A9.mp4",
 './C62DBA36-81A8-4664-8E29-9FCF91248950.mp4', './2E7ED2C4-EB5C-469A-9AEA-07824BD0194C.mp4',
 './521B77C0-DD4C-417E-A842-B9DF6DF343D0.mp4','./3DE96816-A4FF-466B-94BB-2BE1F8604E5B.mp4',
-'./8C8364AC-72DD-46D1-9254-1DBD2668A3AA.mp4',
+'./8C8364AC-72DD-46D1-9254-1DBD2668A3AA.mp4','./E5DAE013-1890-41CA-9DFF-556A276B05CD.MP4',
+'./5AF6D7E0-C02F-43DB-9744-A8D818A1E451.MP4', './5AF6D7E0-C02F-43DB-9744-A8D818A1E451.MP4',
+'./4D0DE805-75F3-45C0-9EE5-EAE40E846C8E.MP4','./export_1664214779019.MP4',
+'./4D7DC781-E5F2-40DF-91D5-0F3E6D565275.MP4','./7D4ED2BD-1040-4417-B945-A722F0785752.MP4',
+'./10D5946A-3EA9-4332-872E-03C075986C9C.MP4', './C37D618A-F14A-486D-AB9D-6B1B3679208A.MP4',
 ];
 const Videogalerie = () => {
  
@@ -1701,10 +1705,10 @@ function prevImage() {
        </video>
        <div className='imgBtn'>
     <button  onClick={prevImage} className='btn1'> 
-      <FontAwesomeIcon icon={faArrowRight} size='1x' style={{color:'white', position:'absolute',transform:'rotate(-180deg)', margin: '0% 57%'}} />
+      <FontAwesomeIcon icon={faArrowRight} size='1x' style={{color:'white', position:'absolute',transform:'rotate(-180deg)', margin: '0% 10%'}} />
     </button>
     <button className='btn1' onClick={nextImage}>
-    <FontAwesomeIcon icon={faArrowRight} style={{color:  'white', position:'absolute', margin:'0% -62%'  }}/>
+    <FontAwesomeIcon icon={faArrowRight} style={{color:  'white', position:'absolute', margin:'0% -10%'  }}/>
   </button>
     </div>
     </div>
